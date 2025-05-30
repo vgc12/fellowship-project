8a08d365cb82c1d81f50e48dc0adf5b0a92c5a53
= <vgc4321@gmail.com>
2025-05-30 11:45:12 -0500

    first commit

---
+++ form_practice.php
@@ -0,0 +1,29 @@
+﻿<?php
+include("include/init.php");
+$comments = getComments();
+debug_output($_REQUEST);
+function insertComment($name, $comment)
+{
+    $now = date_create();
+    $dateTimeString = date_format($now, "Y-m-d H:i:s" );
+
+    db_query("INSERT INTO comments(content, datePosted, name) VALUES ('$comment','$dateTimeString','$name')");
+
+
+}
+
+if(isset($_REQUEST['name']) && isset($_REQUEST['comment'])){
+    insertComment($_REQUEST['name'], $_REQUEST['comment']);
+    header("Location: form_practice.php");
+    exit();
+}
+?>
+
+<html lang="">
+    <form action="" method="post">
+
+        Name: <input type="text" name="name">
+        Comment: <input type="text" name="comment">
+        <input type="submit">
+    </form>
+</html>
--- include/init.php
+++ include/init.php
@@ -2,6 +2,7 @@
 date_default_timezone_set('America/Chicago');
 session_start();
 
+include ('comment.php');
 include('connect.php');
 include('db_query.php');
 include('common_components.php');
