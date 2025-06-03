62ebc570bbd0b1cec6d3e8129d527ce6d80ab7f1
Zaid Abuisba <vgc4321@gmail.com>
2025-06-03 14:02:00 -0500

    initial commit, WebGPU success/failure output into console

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+
+// Check if WebGPU is supported
+if (!navigator.gpu) {
+    console.error("WebGPU is not supported. Please enable it in your browser settings.");
+}
+else {
+    console.log("WebGPU is supported.");
+}
+
+
+
