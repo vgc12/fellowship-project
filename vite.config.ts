362e855ba39b6871e6d5101f7c376c8b7d021947
Zaid Abuisba <vgc4321@gmail.com>
2025-08-17 13:21:21 -0500

    final deployment fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,40 @@
-﻿import { defineConfig } from 'vite';
+﻿import {defineConfig} from 'vite';
 import string from 'vite-plugin-string';
 import react from '@vitejs/plugin-react';
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 
-export default defineConfig({
-    plugins: [
-        tailwindcss(),
-        react(),
-        string({
-            include: '**/*.wgsl', // Specify that .wgsl files should be treated as strings
-            compress: false, // Disable compression to keep the original formatting
-        }),
-    ],
-    resolve: {
-        alias: {
-            "@" : path.resolve(__dirname, "./src")
+export default defineConfig(({command}) =>
+{
+    const base = command === 'build' ? '/webgpu-renderer/' : '/'
+
+    return {
+        plugins: [
+            tailwindcss(),
+            react(),
+            string({
+                include: '**/*.wgsl',
+                compress: false,
+            }),
+        ],
+        base: base,
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "./src")
+            }
+        },
+
+        build: {
+            target: 'esnext',
+            minify: false,
+
+
+        },
+
+
+        optimizeDeps: {
+            include: ['react', 'react-dom', 'react-icons', 'react-icons/*'],
         }
     }
 });
